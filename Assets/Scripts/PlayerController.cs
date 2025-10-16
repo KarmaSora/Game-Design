7701a28fa8aa8b4df66b8429dba3c7fd32f66612
Karam Matar <karammtr@gmail.com>
2025-10-16 13:25:02 +0100

    working on tutorial, added car movement and some collision physics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int carSpeedMultiplier = 90;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.W))
+        //{
+
+        transform.Translate(Vector3.forward * Time.deltaTime * carSpeedMultiplier);   
+        //}
+        
+    }
+}
