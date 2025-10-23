d4cda74b040dc8760d9e827dd69b097d28cac072
Karam Matar <karammtr@gmail.com>
2025-10-23 12:08:56 +0100

    commented code, changed variables from public to private

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,12 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    //changed to private as we dont needthem anymore
+    private float carSpeedMultiplier = 90.0f;
+    private float turnSpeed = 25.0f;
 
-    public float carSpeedMultiplier = 90.0f;
-    public float turnSpeed = 15.0f;
-
-    public float horizontalInput;
-    public float verticalInput;
+    private float horizontalInput;      //Set to private as they are handled  automaticly, can change to public but unneccecery
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +21,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Get Player Input
         horizontalInput = Input.GetAxis("Horizontal");  // horizontal = left and right
         verticalInput = Input.GetAxis("Vertical"); //Vertical means  up and down
 
 
+        //Make Player Move and Rotate
         transform.Translate(Vector3.forward * Time.deltaTime * carSpeedMultiplier * verticalInput);
-
 
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);  // moves the player sideways
         transform.Rotate(Vector3.up,turnSpeed * horizontalInput *Time.deltaTime);  // makes the player rotate
